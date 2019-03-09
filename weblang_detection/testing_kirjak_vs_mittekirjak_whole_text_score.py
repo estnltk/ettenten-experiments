7d2a887f54a902e3727bf8b2fe3b98b0f72908cf
helekal <kaljumae.helen@gmail.com>
2019-03-09 14:36:01 +0200

    Update testing_kirjak_vs_mittekirjak_whole_text_score.py

--- weblang_detection/testing_kirjak_vs_mittekirjak_whole_text_score.py
+++ weblang_detection/testing_kirjak_vs_mittekirjak_whole_text_score.py
@@ -9,22 +9,40 @@ from collections import defaultdict
 all_summaries=[]
 true_category=[]
 
+only_docs_agreement_3 = True # False - use all files; True - use files that were labelled exactly the same by 3 people
+
+def get_info(reader):
+    attr_sum=0
+    for i in row[3:]:
+        number=int(i)
+        attr_sum+=number
+    summary=attr_sum/int(row[2])
+    true_category.append(row[1])
+    all_summaries.append(summary)
+
 with open('weblang_scores.csv', newline='') as f:
     reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
     headers=next(reader)
-    #print(headers)
-    for row in reader:
-        attr_sum=0
-        for i in row[3:]:
-            number=int(i)
-            attr_sum+=number
-        summary=attr_sum/int(row[2])
-        true_category.append(row[1])
-        all_summaries.append(summary)
+    if only_docs_agreement_3==False:
+        for row in reader:
+            get_info(row)
+            
+    if only_docs_agreement_3==True:
+        all_agreed=[]
+        with open('agreement_scores_kirjak_mittekirjak.csv', newline='') as f:
+            reader2 = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+            headers2=next(reader2)
+            for row2 in reader2:
+                if row2[1]=="3":
+                    all_agreed.append(row2[0])
+        for row in reader:
+            if row[0] in all_agreed:
+                get_info(row)
+                            
 
 threshold=[0.038,0.04,0.001,0.025]
 
-print("Files with a summary above threshold are labelled as \"mittekirjak\".")
+print("Files with a summary above threshold are labelled as \"mittekirjak\".\n")
 
 for i in threshold:
     pred_category=[]
